eba096564ef323676940810aace458704f8a1c89
Spencer <Spencer.Taber@yahoo.com>
2021-05-12 17:14:05 +0100

    added position MVP complete

--- src/components/ItunesItem.js
+++ src/components/ItunesItem.js
@@ -3,7 +3,8 @@ import React from 'react';
 
 const ItunesItem = ({itune}) => {
 
-    return <li><h2>{itune['im:name'].label}</h2>{itune['im:artist'].label}{itune.category['im:id']}</li>
+    return <li><h2>{itune['im:name'].label}</h2><p>{itune['im:artist'].label}</p><br></br>
+    {itune.category.attributes.['im:id']}</li>
 }
 
 
