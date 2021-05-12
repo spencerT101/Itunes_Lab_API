ee00d403b9c47997da4f1c604ba60dd5c3d99403
Spencer <Spencer.Taber@yahoo.com>
2021-05-12 15:50:29 +0100

    list component configuration complete

---
+++ src/components/ItunesList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ItunesItem from "./ItunesItem"
+
+
+
+const ItunesList = ({itunes}) => {
+
+    const itunesItems = itunes.map((itune, index) => {
+        return <ItunesItem itune = {itune} key = {index}/>
+    })
+
+    return(
+        <div>
+            <ul>{itunesItems}</ul>
+        </div>
+    )
+}
+
+
+export default ItunesList
--- src/containers/itunesContainer.js
+++ src/containers/itunesContainer.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ItunesList from "../components/ItunesList";
 
 
 
